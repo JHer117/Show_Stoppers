7b0f3514be20d9d04a7d4cbfb324462209d60cad
Jonathan Woldridge <tamarabloodhoof@gmail.com>
2019-12-18 14:40:14 -0800

    Adding Server File.

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from MySQLconnection import connectToMySQL
 import re
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-app.secret_key = "TenaciousJHW"
+app.secret_key = "TenaciousJHJW"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 SESSION_KEY = "user_id"
 if __name__=="__main__":
